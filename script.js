9d9f24647380f6dc74618e0a6e46fef1cf441d72
suot00 <truongkhacsuot661@gmail.com>
2022-07-03 20:02:53 +0700

    new compare

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-function getUserChoice(userChoices) {
-//  get choice user 
- var userChoice = userChoices ; 
-
-//get choice computer
- const computerChoice = getComputerChoice();
-//switch check
- switch (userChoice + computerChoice) {
-        case "sp":
-        case "rs":
-        case "pr":
-            win();
-            break;
-        case "sr":
-        case "rp":    
-        case "ps":
-            lose();
-            break;
-        case "rr":
-        case "pp":
-        case "ss":
-            draw();
-            break;
-}
-
- document.querySelector(".resultUser > span").innerHTML=userChoice ;
- document.querySelector(".resultComputer > span").innerHTML=computerChoice;
-}
-
-//get choice of computer 
-function getComputerChoice() {
-    const choices = ["r", "p", "s"];
+const listValue = [
+    { name: "scissors", icon: "✌",num:1},
+    { name: "rock", icon: "✊",num:2 },
+    { name: "paper", icon: "✋",num:3 }
+  ];
+ 
+// get random computer
+  function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+    return listValue[randomNumber];
 }
-function win() {
-    color="blue";
-    message="Bạn Thắng ";
-    document.querySelector(".textWho").innerHTML =`<h3 style="color: ${color}">${message}</h3>`;
+// check
+const compare = (value1,value2) => {
+      if (value1 == 1 && value2 == 3) {
+        return 1;
+      }
+      if (value1 == 3 && value2 == 1) {
+        return -1;
+      }
+      if (value1 === value2) {
+        return 0;
+      } else {
+        return value1 < value2 ? -1 : 1;
+      }
 }
-
-function lose() {
-    color="red";
-    message="Bạn Thua ";
-    document.querySelector(".textWho").innerHTML = `<h3 style="color: ${color}">${message}</h3>`;
+function getUserChoice(userChoice){
+   var valueUser = 0 ;
+ switch (userChoice) {
+        case "scissors":
+          document.querySelector('.resultUser > span').innerHTML= "✌";
+          valueUser=1;
+            break;
+        case "rock":  
+        document.querySelector('.resultUser > span').innerHTML= "✊";
+        valueUser=2;
+            break;
+        case "paper":
+            document.querySelector('.resultUser > span').innerHTML= "✋";
+            valueUser=3;
+            break;
 }
-
-function draw() {
-    color="green";
-    message="Hòa";
-    document.querySelector(".textWho").innerHTML = `<h3 style="color: ${color}">${message}</h3>`;
+const computerChoice = getComputerChoice();
+ document.querySelector(".resultComputer > span").innerHTML=computerChoice.icon;
+ 
+ const result = compare(valueUser, computerChoice.num);
+ if (result === -1) {
+    message = "Bạn đã thua";
+    document.querySelector('.textWho').innerHTML=message;
+  } else if (result === 0) {
+    message = "Bạn đã hòa"; 
+    document.querySelector('.textWho').innerHTML=message;
+  } else {
+    message = "Bạn đã thắng";
+    document.querySelector('.textWho').innerHTML=message;
+  }
+  
 }
-   
 
